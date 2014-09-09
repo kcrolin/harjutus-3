629f078d59d0e4f38066fc5c20fdd8a343cffbb7
kcrolin <carolin.karing@khk.ee>
2014-09-09 15:34:48 +0100

    2
    Add name variables

--- index.php
+++ index.php
@@ -20,17 +20,37 @@
 	  echo "Hello world!" . "<br>";
 	  echo 'Hello world!';
 	  echo "<br>";
+	  
 	  $var = "24";
 	  echo $var;
 	  echo "<br>";
+	  
 	  $var2 = "26";
 	  echo $var2;
 	  echo "<br>";
+	  
 	  echo $var . $var2;
     ?>
 	
+	<h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
 	
-	
+	 <?php 
+	  $student1 = "Mihkel";
+	  $student2 = "Heiko";
+	  $student3 = "Marko";
+	  $student4 = "Ingrid";
+	  $student5 = "Eve";
+	  ?> 
+	  
+	  <ul>
+	    <li> <?php echo $student1; ?> </li>
+		<li> <?php echo $student2; ?> </li>
+		<li> <?php echo $student3; ?> </li>
+		<li> <?php echo $student4; ?> </li>
+		<li> <?php echo $student5; ?> </li>
+	  </ul>
+	  
+	  
 	  
 </body>
 </html>
